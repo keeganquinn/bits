abff6b0f7d112342b6140a6e383fec064873ff39
Keegan Quinn <keeganquinn@gmail.com>
2016-12-09 16:12:40 -0800

    Remove unwanted domains.

--- config/apache/ni-guild.ml
+++
@@ -1,7 +0,0 @@
-<VirtualHost *:80>
-    ServerName ni-guild.ml
-    ServerAlias www.ni-guild.ml
-    ServerAdmin webmaster@ni-guild.ml
-
-    RedirectMatch 301 ^/(.*)$ http://abusive.gq/$1
-</VirtualHost>
