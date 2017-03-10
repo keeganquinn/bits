690ef4a87de634cfac4f62174356e689e5bf5f1c
Keegan Quinn <keeganquinn@gmail.com>
2017-03-09 22:20:31 -0800

    Update page template.

--- js/page.js
+++ js/page.js
@@ -1,8 +1,3 @@
-/**
- * bits: common JavaScript
- * © 2014 Keegan Quinn
- */
-
 $(document).ready(function() {
   // Open all external links in new tabs/windows
   $("a[href^='http://']").attr("target", "_blank");
