7b278f6553fe9de50a1ded82c2b8dea0da4402da
Keegan Quinn <keeganquinn@gmail.com>
2018-02-11 06:37:38 -0800

    Ensure that the ssh agent has at least one identity.

--- fire.sh
+++ fire.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+ssh-add -l >/dev/null || ssh-add
+
+read -rs -p 'Vault password: ' vault_password
+echo
+
+
 scratch=$(mktemp -d)
 chmod 0700 "${scratch}"
 function cleanup {
@@ -12,9 +18,6 @@ function cleanup {
     exit
 }
 trap cleanup INT TERM
-
-read -rs -p 'Vault password: ' vault_password
-echo
 
 export ANSIBLE_VAULT_PASSWORD_FILE="${scratch}/vault_password"
 echo "${vault_password}" > "${ANSIBLE_VAULT_PASSWORD_FILE}"
