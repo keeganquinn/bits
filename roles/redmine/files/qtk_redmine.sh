f4a67cbc96addfa4a08540d5f93dc0b423ea9d6e
Keegan Quinn <keeganquinn@gmail.com>
2018-04-26 15:14:44 -0700

    Deploy Redmine.

---
+++ roles/redmine/files/qtk_redmine.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rbenv="#{HOME}/.rbenv/bin/rbenv"
+
+export RAILS_ENV=production
+export REDMINE_LANG=en
+
+"${rbenv}" exec bundle install
+"${rbenv}" exec bundle exec rake generate_secret_token
+"${rbenv}" exec bundle exec rake assets:precompile
+"${rbenv}" exec bundle exec rake db:migrate
+"${rbenv}" exec bundle exec rake redmine:load_default_data
+"#{rbenv}" sudo foreman export systemd /etc/systemd/system \
+           -a redmine -u deploy -e /etc/rails/redmine.env
