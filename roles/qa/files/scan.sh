e21d232bdc85fdcc503000530f29e569ef3234a7
Keegan Quinn <keeganquinn@gmail.com>
2018-06-07 04:04:43 -0700

    Add qa role with scheduled dependency scan script.

---
+++ roles/qa/files/scan.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Scan for updated libraries/dependencies on all qtk software packages.
+
+set -e
+
+scratch=$(mktemp -d)
+chmod 0700 "${scratch}"
+cleanup() {
+    rm -rf "${scratch}"
+    exit
+}
+trap cleanup INT TERM
+
+
+# animamagica
+git clone -q /srv/data/git/animamagica.git "${scratch}/animamagica"
+if ! yarn outdated --cwd "${scratch}/animamagica" >/dev/null; then
+    echo 'Anima Magica JS dependencies need update!'
+    echo
+fi
+if ! (cd "${scratch}/animamagica"; bundle outdated) >/dev/null; then
+    echo 'Anima Magica Ruby dependencies need update!'
+    echo
+fi
+rm -rf "${scratch}/animamagica"
+
+
+# cwnmyr
+git clone -q https://github.com/keeganquinn/cwnmyr.git "${scratch}/cwnmyr"
+if ! yarn outdated --cwd "${scratch}/cwnmyr" >/dev/null; then
+    echo 'cwnmyr JS dependencies need update!'
+    echo
+fi
+if ! (cd "${scratch}/cwnmyr"; bundle outdated) >/dev/null; then
+    echo 'cwnmyr Ruby dependencies need update!'
+    echo
+fi
+rm -rf "${scratch}/cwnmyr"
+
+
+# partystarter
+git clone -q /srv/data/git/partystarter.git "${scratch}/partystarter"
+if ! yarn outdated --cwd "${scratch}/partystarter" >/dev/null; then
+    echo 'PartyStarter JS dependencies need update!'
+    echo
+fi
+if ! (cd "${scratch}/partystarter"; bundle outdated) >/dev/null; then
+    echo 'PartyStarter Ruby dependencies need update!'
+    echo
+fi
+rm -rf "${scratch}/partystarter"
+
+cleanup
