cb290f4162e99b73b54f28fe59f0c02203186e01
Keegan Quinn <keeganquinn@gmail.com>
2019-03-08 21:13:50 -0800

    Update to Mastodon 2.7.4.

--- roles/social/files/setk_mastodon.sh
+++ roles/social/files/setk_mastodon.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 rbenv="${HOME}/.rbenv/bin/rbenv"
 
 export RAILS_ENV=production
 
 "${rbenv}" exec bundle install
+"${rbenv}" exec bundle exec rake assets:precompile
+"${rbenv}" exec bundle exec rake db:migrate
 "${rbenv}" sudo foreman export systemd /etc/systemd/system \
            -a mastodon -u deploy -e /etc/rails/mastodon.env
 
