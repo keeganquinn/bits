8383b78018ed94e81be619643f5dd200270640d1
Keegan Quinn <keeganquinn@gmail.com>
2019-03-20 16:23:53 -0700

    Schedule database backups via cron instead of rsnapshot.

--- roles/rails/files/pg_backup.sh
+++ roles/rails/files/pg_backup.sh
@@ -2,10 +2,15 @@
 
 set -ex
 
-pg_dumpall -g | gzip > "globals".sql.gz
+DEST=/var/backups/pg
+
+mkdir -p "${DEST}"
+rm -f "${DEST}"/*
+
+pg_dumpall -g | gzip > "${DEST}/globals.sql.gz"
 
 BACKUP_QUERY="select datname from pg_database where not datistemplate and datallowconn order by datname;"
 
 for DATABASE in $(psql -At -c "$BACKUP_QUERY" postgres); do
-    pg_dump -Fc "$DATABASE" -f "$DATABASE".pgc
+    pg_dump -Fc "$DATABASE" -f "${DEST}/${DATABASE}.pgc"
 done
