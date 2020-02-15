632fe8505aa8ffa8f8806ac53cf436a00255a0ca
Keegan Quinn <keegan@quinn.tk>
2020-02-14 18:17:35 -0800

    Add reminders to update deployed software.

--- roles/qa/files/scan.sh
+++ roles/qa/files/scan.sh
@@ -40,3 +40,8 @@ for pspec in "${PROJECTS[@]}"; do
 done
 
 cleanup
+
+echo "Ruby: 2.7.0 - check https://www.ruby-lang.org/"
+echo "Redmine: 4.1.0 - check https://redmine.org/"
+echo "Roundcube: 1.4.2 - check https://roundcube.net/"
+echo "Nextcloud: check https://cloud.quinn.tk/settings/admin/overview"
