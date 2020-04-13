5708261d2c9b2b7e3bd7eb30e7e3bb45685acc27
Keegan Quinn <keegan@quinn.tk>
2020-04-13 16:03:33 -0700

    Update to Redmine 4.1.1.

--- roles/qa/files/scan.sh
+++ roles/qa/files/scan.sh
@@ -40,7 +40,7 @@ for pspec in "${PROJECTS[@]}"; do
 done
 
 echo "Ruby: 2.7.1 - check https://www.ruby-lang.org/"
-echo "Redmine: 4.1.0 - check https://redmine.org/"
+echo "Redmine: 4.1.1 - check https://redmine.org/"
 echo "Roundcube: 1.4.3 - check https://roundcube.net/"
 echo "Nextcloud: check https://cloud.quinn.tk/settings/admin/overview"
 
