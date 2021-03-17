5812b7b7b82979c45d6106de921d3e94887112aa
Keegan Quinn <keegan@qtk.io>
2021-03-16 23:24:14 -0700

    Bump Roundcube version.

--- roles/qa/files/scan.sh
+++ roles/qa/files/scan.sh
@@ -40,7 +40,7 @@ done
 
 echo "Ruby: 2.7.2 - check https://www.ruby-lang.org/"
 echo "Redmine: 4.1.1 - check https://redmine.org/"
-echo "Roundcube: 1.4.10 - check https://roundcube.net/"
+echo "Roundcube: 1.4.11 - check https://roundcube.net/"
 echo "Nextcloud: check https://cloud.qtk.io/settings/admin/overview"
 
 cleanup
